791a2599dbe01081a0c4af529216cc2ea756e5fd
Benjamin D. Peterson <petersonben50@gmail.com>
2020-04-21 15:14:22 -0500

    Set up leucine cleaning script to calculate BPP and BCP for one date

--- code/cleaning_scripts/clean_leucine_uptake.R
+++ code/cleaning_scripts/clean_leucine_uptake.R
@@ -1,9 +1,9 @@
 #### code/cleaning_scripts/clean_leucine_uptake.R ####
 # Benjamin D. Peterson
 
-#### Your mom isn't here, clean this shit up ####
+#### Your mom isn't here, and even if she was she shouldn't have to clean up after you ####
 rm(list = ls())
-setwd("~/Box/BLiMMP/")
+setwd("/Users/benjaminpeterson/Documents/research/BLiMMP")
 library(readxl)
 library(tidyverse)
 
@@ -38,7 +38,7 @@ metadata.MA <- leucine.metadata %>%
                            " - ",
                            filtered.vector[filtered],
                            sep = "")) %>%
-  select(uptakeID, incubationID, startDate, depth, condition)
+  select(uptakeID, incubationID, sampleID, startDate, depth, condition)
 
 rm(list = ls(pattern = ".metadata"),
    filtered.vector)
@@ -49,36 +49,86 @@ rm(list = ls(pattern = ".metadata"),
 efficiency.data <- read_xlsx("dataRaw/leucineUptake/20191009_leucine.xlsx") %>%
   filter(Condition == "efficiency")
 
+# We used 1µl of a 2µM leucine solution for our efficiency test.
+# Therefore, we'll divide the number of counts that we got to 
+# calculate the overall efficiency of the counts, which in this
+# case means the counts that we get per leucine in solution.
+# This will provide a direct link between the counts we got and
+# the amount of leucine taken up.
+
 # 2µM leucine solution -> 2pmol/ul
 # 1µl volume
-leucine.pmol.eff <- 2*1
+leucine.pmol <- 2*1
 
-efficiency.data <- efficiency.data %>%
-  mutate(CF_A = CPMA / leucine.pmol.eff) %>%
-  mutate(CF_B = CPMB / leucine.pmol.eff) %>%
+efficiency.data.count.per.pmol <- efficiency.data %>%
+  mutate(CF_A = CPMA / leucine.pmol) %>%
+  mutate(CF_B = CPMB / leucine.pmol) %>%
   select(CF_A, CF_B) %>%
   colMeans()
 
-rm(leucine.pmol.eff)
+rm(efficiency.data, leucine.pmol)
+
+
 
 #### Read in and clean data: 2019-10-14 ####
 
-incubation.time.hours.temp <- 90/60
-incubation.volume.L.temp <- 0.001
+# I'll walk through the first one closely with notes
 
+# First we set the time that each of these incubated for.
+incubation.time.hours.temp <- 90/60
+
+# We also need the total volume of the incubation in L.
+incubation.volume.mL.temp <- 1
+incubation.volume.L.temp <- incubation.volume.mL.temp / 1000
+rm(incubation.volume.mL.temp)
+
+# I'm assuming that the ID of leucine is 2, as described by Simon and Azam, 1989.
+leucine.ID <- 2
+
+# Set the molar ratio of leucine in protein as 7.3%, according to Exercise 19 in
+# Limnological Analyses by Wetzel and Likens. This ratio was determined in Simon
+# and Azam, 1989
+Leu.per.prot.molar.ratio <- 0.073
+
+# Set molar mass leucine
+molar.mass.leucine <- 131.2
+
+# Set cellular carbon per protein
+cell_C_to_protein <- 0.86
+
+# Then we'll read in the raw data
 leucine.data.temp <- read_xlsx("dataRaw/leucineUptake/20191014_leucine.xlsx") 
 
+# Calculate the blank values
 blank.data.temp <- leucine.data.temp %>%
   filter(uptakeID == "negative_control") %>%
   select(CPMA, CPMB) %>%
   colMeans()
-  
+
+# Now for the calculations.
+# For both A and B counting channels, we'll first take the counts and subtract out the blank values.
+# Then we'll divide the counts by the efficiency data, to calculate the pmol of leucine that have been
+# taken up. Then we'll divide by the incubation volume to get pmol/L. And then we'll divide it by
+# the number of hours of incubation, to normalize production to 1 hour.
+# This gives us the pM of leucine incorporated per hour.
+
 incubation.data.20191014 <- leucine.data.temp %>%
-  mutate(Leu_uptake_pM_A = (CPMA - blank.data.temp["CPMA"]) / (incubation.time.hours.temp * incubation.volume.L.temp * efficiency.data["CF_A"])) %>%
-  mutate(Leu_uptake_pM_B = (CPMB - blank.data.temp["CPMB"]) / (incubation.time.hours.temp * incubation.volume.L.temp * efficiency.data["CF_B"])) %>%
-  mutate(ratio_A_B = Leu_uptake_pM_A / Leu_uptake_pM_B) %>%
+  mutate(Leu_uptake_pM_A = (CPMA - blank.data.temp["CPMA"]) / (efficiency.data.count.per.pmol["CF_A"] * incubation.volume.L.temp * incubation.time.hours.temp)) %>%
+  mutate(Leu_uptake_pM_B = (CPMB - blank.data.temp["CPMB"]) / (efficiency.data.count.per.pmol["CF_B"] * incubation.volume.L.temp * incubation.time.hours.temp)) %>%
+  #mutate(ratio_A_B = Leu_uptake_pM_A / Leu_uptake_pM_B) %>%
+  
+  # To convert this to overall pM of protein produced per house, we divide the pM of leucine by the
+  # molar ratio of leucine in protein, which is approximately 7.3% and multiply by the molar mass of 
+  # leucine and the leucine isotope dilution, which here we are assuming is 2.
+  # I think this is a BS way of doing it, since the 7.3% value is a molar ratio, not a mass ratio.
+  # But, this is how people have been calculating it forever, so I'm gonna stick with it. At some point 
+  # I should use Simon and Azam's tables to calculate the mass ratio.
+  mutate(µgBPP_per_L_hr = round(((Leu_uptake_pM_A / Leu.per.prot.molar.ratio) * molar.mass.leucine * leucine.ID) / 1000000, 3)) %>%
+  
+  # The last conversion to make is from protein biomass to overall carbon biomass.
+  mutate(µgBCP_per_L_hr = µgBPP_per_L_hr * cell_C_to_protein) %>%
   filter(Condition == "ambient") %>%
-  select(c(uptakeID, Leu_uptake_pM_A))
+  select(c(uptakeID, Leu_uptake_pM_A, µgBPP_per_L_hr, µgBCP_per_L_hr))
 
 rm(list = ls(pattern = ".temp"))
 
@@ -115,8 +165,8 @@ blank.data.temp <- leucine.data.temp %>%
 WC.data.20191009 <- leucine.data.temp %>%
   filter(Condition == "ambient") %>%
   left_join(blank.data.temp) %>%
-  mutate(Leu_uptake_pM_A = (CPMA - blank_CPMA) / (incubation.time.hours.temp * incubation.volume.L.temp * efficiency.data["CF_A"])) %>%
-  mutate(Leu_uptake_pM_B = (CPMB - blank_CPMB) / (incubation.time.hours.temp * incubation.volume.L.temp * efficiency.data["CF_B"])) %>%
+  mutate(Leu_uptake_pM_A = (CPMA - blank_CPMA) / (efficiency.data.count.per.pmol["CF_A"] * incubation.volume.L.temp * incubation.time.hours.temp)) %>%
+  mutate(Leu_uptake_pM_B = (CPMB - blank_CPMB) / (efficiency.data.count.per.pmol["CF_B"] * incubation.volume.L.temp * incubation.time.hours.temp)) %>%
   mutate(ratio_A_B = Leu_uptake_pM_A / Leu_uptake_pM_B) %>%
   select(c(uptakeID, sampleID, startDate, depth, Leu_uptake_pM_A))
 
@@ -154,8 +204,8 @@ blank.data.temp <- leucine.data.temp %>%
 WC.data.20190831 <- leucine.data.temp %>%
   filter(Condition == "ambient") %>%
   left_join(blank.data.temp) %>%
-  mutate(Leu_uptake_pM_A = (CPMA - blank_CPMA) / (incubation.time.hours.temp * incubation.volume.L.temp * efficiency.data["CF_A"])) %>%
-  mutate(Leu_uptake_pM_B = (CPMB - blank_CPMB) / (incubation.time.hours.temp * incubation.volume.L.temp * efficiency.data["CF_B"])) %>%
+  mutate(Leu_uptake_pM_A = (CPMA - blank_CPMA) / (efficiency.data.count.per.pmol["CF_A"] * incubation.volume.L.temp * incubation.time.hours.temp)) %>%
+  mutate(Leu_uptake_pM_B = (CPMB - blank_CPMB) / (efficiency.data.count.per.pmol["CF_B"] * incubation.volume.L.temp * incubation.time.hours.temp)) %>%
   mutate(ratio_A_B = Leu_uptake_pM_A / Leu_uptake_pM_B) %>%
   select(c(uptakeID, sampleID, startDate, depth, Leu_uptake_pM_A))
 
