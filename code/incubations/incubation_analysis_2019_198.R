797e05a4171e1c81162fcfa5496df32bf1c6d7bf
Benjamin D. Peterson <petersonben50@gmail.com>
2020-04-28 17:54:50 -0500

    Added scripts to plot change in % MeHg for 198Hg

--- code/incubations/incubation_analysis_2019_198.R
+++ code/incubations/incubation_analysis_2019_198.R
@@ -251,6 +251,7 @@ empty.plot <- function(x) {
        xaxt = "n",
        yaxt = "n",
        xlab = "",
+       ylab = "",
        bty = "n")
 }
 
@@ -397,7 +398,7 @@ color.vector.003 <- c(cb.translator["bluishgreen"],
 names(color.vector.003) <- c("unfiltered-unamended",
                              "filtered-unamended")
 
-png("results/incubations/BLiMMP_trip_003_incubations.png",
+png("results/incubations/methylation/incubations2019/BLiMMP_trip_003_incubations.png",
     units = "in",
     res = 240,
     height = 4,
@@ -415,7 +416,7 @@ plot.incubation.data(trip.of.interest = "BLiMMP_trip_003",
 dev.off()
 
 
-png("results/incubations/BLiMMP_trip_005_incubations.png",
+png("results/incubations/methylation/incubations2019/BLiMMP_trip_005_incubations.png",
     units = "in",
     res = 240,
     height = 10,
@@ -446,7 +447,7 @@ names(color.vector.006) <- c("unfiltered-molybdate",
                          "unfiltered-starch-molybdate",
                          "unfiltered-algal")
 
-png("results/incubations/BLiMMP_trip_006_incubations.png",
+png("results/incubations/methylation/incubations2019/BLiMMP_trip_006_incubations.png",
     units = "in",
     res = 240,
     height = 10,
@@ -462,3 +463,300 @@ plot.incubation.data(trip.of.interest = "BLiMMP_trip_006",
                      empty.plots = NULL,
                      color.vector.for.Hg = color.vector.006)
 dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Function to plot percent Me198Hg at each time point for each depth on a given trip ####
+
+per.MeHg.data <- read.csv("dataEdited/incubations/MeHg/incubations2019_Me198Hg_percent.csv",
+                          stringsAsFactors = FALSE)
+
+plot.per.Me198Hg.time.course.depth <- function(selected.depth,
+                                               trip_data,
+                                               color.vector.input = NULL) {
+  
+  par(mar=c(3,3,2.5,1), mgp=c(1.5,0.4,0), tck=-0.008)
+  
+  y_max <- ceiling(max(trip_data$per_MeHg_198)*100)/100 + 0.01
+  y_min <- floor(min(trip_data$per_MeHg_198)*100)/100
+  
+  # Isolate by depth
+  trip_data_depth <- trip_data %>%
+    filter(depth == selected.depth)
+  
+  #### Make a color vector ####
+  if (is.null(color.vector.input)) {
+    color.vector <- c(cb.translator["black"],
+                      cb.translator["bluishgreen"],
+                      cb.translator["vermillion"])
+    names(color.vector) <- c("unfiltered-molybdate",
+                             "unfiltered-unamended",
+                             "filtered-unamended")
+  } else {
+    color.vector <- color.vector.input
+  }
+  
+  ####  Make a time vector #### 
+  time.vector <- c(0, 1)
+  names(time.vector) <- c("t0", "t1")
+  
+  
+  plot(x = time.vector[trip_data_depth$t],
+       y = trip_data_depth$per_MeHg_198,
+       ylim = c(y_min, y_max),
+       xlim = c(-0.5, 1.5),
+       pch = 18,
+       col = color.vector[trip_data_depth$treatment],
+       cex = 1.25,
+       xaxt = "n",
+       xlab = "",
+       ylab = "",
+       main = paste(unique(trip_data_depth$startDate),
+                    ": ",
+                    selected.depth,
+                    "m",
+                    sep = ""))
+  axis(1,
+       at = c(0, 1),
+       labels = c("t0", "t1"))
+  
+  #### Add segments to link samples within an incubation ####
+  
+  trip_data_depth_segments <- trip_data_depth %>%
+    select(-depth) %>%
+    spread(key = t,
+           value = per_MeHg_198)
+  
+  for (row.num in 1:nrow(trip_data_depth_segments)) {
+    segments(0, trip_data_depth_segments$t0[row.num],
+             1, trip_data_depth_segments$t1[row.num],
+             col = color.vector[trip_data_depth_segments$treatment[row.num]])
+  }
+  
+  title(ylab = "Fraction MeHg for 198Hg",
+        line = 1.75)
+  
+  legend("topleft",
+         legend =  names(color.vector)[length(color.vector):1],
+         text.col = color.vector[length(color.vector):1],
+         bty = "n")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#### Function to run plotting function over each depth ####
+
+trip.of.interest <- "BLiMMP_trip_006"
+plot.per.MeHg.changes <- function(trip.of.interest,
+                                  color.input = NULL) {
+  trip_data_df <- per.MeHg.data %>%
+    filter(tripID == trip.of.interest) %>%
+    select(incubationID, depth, startDate, t, treatment, per_MeHg_198)
+  sapply(unique(trip_data_df$depth) %>% sort(),
+         function(depth) {
+           plot.per.Me198Hg.time.course.depth(selected.depth = depth,
+                                              trip_data = trip_data_df,
+                                              color.vector.input = color.input)
+         }
+  )
+}
+
+
+
+
+
+
+
+
+
+
+#### Generate images ####
+
+png("results/incubations/methylation/incubations2019/BLiMMP_003_per_Me198Hg.png",
+    units = "in",
+    res = 240,
+    width = 8,
+    height = 5)
+par(mfrow = c(1, 2))
+plot.per.MeHg.changes("BLiMMP_trip_003")
+dev.off()
+
+png("results/incubations/methylation/incubations2019/BLiMMP_005_per_Me198Hg.png",
+    units = "in",
+    res = 240,
+    width = 8,
+    height = 5)
+par(mfrow = c(1, 3))
+plot.per.MeHg.changes("BLiMMP_trip_005")
+dev.off()
+
+png("results/incubations/methylation/incubations2019/BLiMMP_006_per_Me198Hg.png",
+    units = "in",
+    res = 240,
+    width = 8,
+    height = 5)
+par(mfrow = c(1, 2))
+plot.per.MeHg.changes("BLiMMP_trip_006",
+                      color.input = color.vector.006)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Change in Me198Hg over incubation ####
+
+Me198Hg.per.change <- read.csv("dataEdited/incubations/MeHg/incubations2019_Me198Hg_change_in_percent.csv",
+                               stringsAsFactors = FALSE)
+
+
+# Generate needed color vector
+color.vector.003 <- c(cb.translator["vermillion"],
+                      cb.translator["bluishgreen"])
+names(color.vector.003) <- c("filtered\nunamended",
+                             "unfiltered\nunamended")
+
+color.vector.005 <- c(cb.translator["vermillion"],
+                      cb.translator["bluishgreen"],
+                      cb.translator["black"])
+names(color.vector.005) <- c("filtered\nunamended",
+                             "unfiltered\nunamended",
+                             "unfiltered\nmolybdate")
+
+color.vector.006 <- c(cb.translator["black"],
+                      cb.translator["bluishgreen"],
+                      cb.translator["vermillion"],
+                      cb.translator["orange"],
+                      cb.translator["blue"],
+                      cb.translator["reddishpurple"])
+names(color.vector.006) <- c("unfiltered\nmolybdate",
+                             "unfiltered\nunamended",
+                             "filtered\nunamended",
+                             "unfiltered\nstarch",
+                             "unfiltered\nstarch\nmolybdate",
+                             "unfiltered\nalgal")
+
+
+plot.date.data <- function(date.of.interest,
+                           color.vector) {
+  
+  per.change.MeHg.data.date <- Me198Hg.per.change %>%
+    filter(startDate == date.of.interest)
+  
+  for (depth.number in 1:length(unique(per.change.MeHg.data.date$depth))) {
+    
+    depth.vector <- unique(per.change.MeHg.data.date$depth) %>% sort()
+    depth.of.interest <- depth.vector[depth.number]
+    
+    depth.of.interest.m <- paste(depth.of.interest, "m", sep = "")
+    
+    plot.list[[depth.number]] <- per.change.MeHg.data.date %>%
+      filter(depth == depth.of.interest) %>%
+      mutate(treatment = gsub("-", "\n", treatment)) %>%
+      mutate(treatment = factor(treatment, levels = names(color.vector))) %>%
+      ggplot(aes(x = treatment,
+                 y = change_in_per_MeHg_norm,
+                 color = treatment)) +
+      geom_jitter(position = position_jitter(0.1)) +
+      scale_color_manual(values = color.vector) +
+      theme_classic() +
+      theme(legend.position = "none") +
+      ylim(min(per.change.MeHg.data.date$change_in_per_MeHg_norm)-5,
+           max(per.change.MeHg.data.date$change_in_per_MeHg_norm)+5) +
+      xlab("") +
+      ylab("Change in % Me198Hg") +
+      ggtitle(paste(date.of.interest,
+                    ": ",
+                    depth.of.interest.m,
+                    sep = ""))
+    
+  }
+  
+  if (length(plot.list) == 2) {
+    plot.list[[3]] <- ggplot() + theme_void()
+  }
+  
+  plot.list[[1]] + plot.list[[2]] + plot.list[[3]]
+  
+}
+
+
+
+#### Read out plots ####
+
+png("results/incubations/methylation/incubations2019/BLiMMP_003_change_in_per_Me198Hg.png",
+    units = "in",
+    res = 240,
+    width = 8,
+    height = 4)
+plot.list <- list()
+plot.date.data("2019-07-30",
+               color.vector = color.vector.003)
+dev.off()
+
+png("results/incubations/methylation/incubations2019/BLiMMP_005_change_in_per_Me198Hg.png",
+    units = "in",
+    res = 240,
+    width = 8,
+    height = 4)
+plot.list <- list()
+plot.date.data("2019-08-30",
+               color.vector = color.vector.005)
+dev.off()
+
+
+png("results/incubations/methylation/incubations2019/BLiMMP_006_change_in_per_Me198Hg.png",
+    units = "in",
+    res = 240,
+    width = 8,
+    height = 4)
+plot.list <- list()
+plot.date.data("2019-10-08",
+               color.vector = color.vector.006)
+dev.off()
+
