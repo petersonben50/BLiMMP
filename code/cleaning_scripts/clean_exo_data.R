6768b77d53a15d7ede4b32f5b788dabec983a82f
Benjamin D. Peterson <petersonben50@gmail.com>
2021-04-16 22:51:38 -0500

    Scripts to clean Exo data

---
+++ code/cleaning_scripts/clean_exo_data.R
@@ -0,0 +1,162 @@
+#### code/cleaning_scripts/clean_exo_data.R ####
+# Benjamin D. Peterson
+
+# This scripts reads in the sonde data collected on the Exo
+# sonde. The data was collected as part of the BLiMMP project.
+# These samples were collected from Lake Mendota
+
+
+#### This is your mess, you need to clean it up ####
+rm(list = ls())
+setwd("~/Documents/research/BLiMMP")
+library(dplyr)
+library(readxl)
+
+
+#### 2019-08-21 ####
+# Read in data
+exo.data <- read_xlsx("dataRaw/exo/EXO_SD_FLAME_13E101468_082119_125251.xlsx",
+                      skip = 22) %>%
+  as.data.frame()
+
+# Clean up datafile
+# Read in custom headers that I made (found in sonde_profile_headers.csv)
+sonde.names <- c("date", "time", "time_fraction", "site_name", "fault_code", "Battery_V",
+                 "Cable_Pwr_V", "Chlorophyll_RFU", "Chlorophyll_µg.L", "BGA-PC_RFU", "BGA-PC_µg.L",
+                 "ODO_sat", "ODO_conc", "fDOM_RFU", "fDOM_QSU", "Turbidity_FNU", "TSS_mg.L",
+                 "Temp_C", "Cond_µS.cm", "SpCond_µS.cm", "Sal_psu", "nLF_Cond_µS.cm", "TDS_mg.L",
+                 "Press_psi_a", "Depth_m")
+# Add names, clean the data
+colnames(exo.data) <- sonde.names
+# Start at 1 meters
+exo.data <- exo.data[which(exo.data$Depth_m >= 1), ]
+# End at maximum depth
+exo.data <- exo.data[1:which.max(exo.data$Depth_m), ]
+# Order by depth and time
+exo.data <- exo.data[order(exo.data$Depth_m, exo.data$time), ]
+# Round depths to nearest 0.1m
+exo.data$Depth_m <- round(exo.data$Depth_m,
+                                       digits = 1)
+# Keep only columns of interest
+exo.data <- exo.data %>%
+  select(Temp_C, Cond_µS.cm, SpCond_µS.cm, Sal_psu,
+         ODO_sat, ODO_conc, fDOM_RFU,
+         Turbidity_FNU, Chlorophyll_RFU, Depth_m)
+# Aggregate data by depth
+exo.data <- aggregate(exo.data,
+                      by = list(exo.data$Depth_m),
+                      FUN = mean)
+
+# Write out csv of data
+write.csv(exo.data,
+          file = "dataEdited/exo/2019-08-21_profile.csv",
+          row.names = FALSE)
+rm(exo.data)
+
+#### 2019-08-30 ####
+# Read in data
+exo.data.2 <- read_xlsx("dataRaw/exo/EXO_SD_FLAME_13E101468_083019_191108.xlsx",
+                        skip = 23) %>%
+  as.data.frame()
+
+# Clean up datafile
+# Read in custom headers that I made (found in sonde_profile_headers.csv)
+sonde.names <- c("date", "time", "time_fraction", "site_name", "fault_code", "Battery_V",
+                 "Cable_Pwr_V", "Temp_C", "Cond_µS.cm", "SpCond_µS.cm", "Sal_psu",
+                 "nLF_Cond_µS.cm", "TDS_mg.L", "ODO_sat", "ODO_conc", "fDOM_RFU", "fDOM_QSU",
+                 "pH", "pH_mV", "ORP_mV", "ORP_raw_mV", "Turbidity_FNU", "TSS_mg.L",
+                 "Chlorophyll_RFU", "Chlorophyll_µg.L", "BGA-PC_RFU", "BGA-PC_µg.L",
+                 "Press_psi_a", "Depth_m")
+# Add names, clean the data
+colnames(exo.data.2) <- sonde.names
+# Start at 1 meters
+exo.data.2 <- exo.data.2[which(exo.data.2$Depth_m >= 1), ]
+# End at maximum depth
+exo.data.2 <- exo.data.2[1:which.max(exo.data.2$Depth_m), ]
+# Order by depth and time
+exo.data.2 <- exo.data.2[order(exo.data.2$Depth_m, exo.data.2$time), ]
+# Round depths to nearest 0.1m and average these measurements
+exo.data.2$Depth_m <- round(exo.data.2$Depth_m,
+                            digits = 1)
+exo.data.2 <- exo.data.2 %>%
+  select(Temp_C, Cond_µS.cm, SpCond_µS.cm, Sal_psu,
+         ODO_sat, ODO_conc, fDOM_RFU, pH, ORP_mV,
+         Turbidity_FNU, Chlorophyll_RFU, Depth_m)
+exo.data.2 <- aggregate(exo.data.2,
+                        by = list(exo.data.2$Depth_m),
+                        FUN = mean)
+
+# Write out csv of data
+write.csv(exo.data.2,
+          file = "dataEdited/exo/2019-08-30_profile.csv",
+          row.names = FALSE)
+
+#### 2019-10-08 ####
+# Read in data
+exo.data.3 <- read_xlsx("dataRaw/exo/EXO_SD_FLAME_13E101468_100819_183725.xlsx",
+                        skip = 24) %>%
+  as.data.frame()
+
+# Clean up datafile
+# Read in custom headers that I made (found in sonde_profile_headers.csv)
+sonde.names <- c("date", "time", "time_fraction", "site_name", "fault_code", "Battery_V",
+                 "Cable_Pwr_V", "Temp_C", "Cond_µS.cm", "SpCond_µS.cm", "Sal_psu",
+                 "nLF_Cond_µS.cm", "TDS_mg.L", "ODO_sat", "ODO_conc", "fDOM_RFU", "fDOM_QSU",
+                 "pH", "pH_mV", "ORP_mV", "ORP_raw_mV", "Turbidity_FNU", "TSS_mg.L",
+                 "Chlorophyll_RFU", "Chlorophyll_µg.L", "BGA-PC_RFU", "BGA-PC_µg.L",
+                 "Press_psi_a", "Depth_m")
+# Add names, clean the data
+colnames(exo.data.3) <- sonde.names
+# Start at 1 meters
+exo.data.3 <- exo.data.3[which(exo.data.3$Depth_m >= 1), ]
+# End at maximum depth
+exo.data.3 <- exo.data.3[1:which.max(exo.data.3$Depth_m), ]
+# Order by depth and time
+exo.data.3 <- exo.data.3[order(exo.data.3$Depth_m, exo.data.3$time), ]
+# Round depths to nearest 0.1m and average these measurements
+exo.data.3$Depth_m <- round(exo.data.3$Depth_m,
+                            digits = 1)
+exo.data.3 <- exo.data.3 %>%
+  select(Temp_C, Cond_µS.cm, SpCond_µS.cm, Sal_psu,
+         ODO_sat, ODO_conc, fDOM_RFU, pH, ORP_mV,
+         Turbidity_FNU, Chlorophyll_RFU, Depth_m)
+exo.data.3 <- aggregate(exo.data.3,
+                        by = list(exo.data.3$Depth_m),
+                        FUN = mean) %>%
+  select(-Group.1)
+
+# Write out csv of data
+write.csv(exo.data.3,
+          file = "dataEdited/exo/20191008_profile.csv",
+          row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+#### 2020 data ####
+rm(list = ls())
+source("code/cleaning_scripts/cleaning_functions.R")
+
+clean.exo.data(file.name = "exo_profile_ME20200807a.xlsx",
+               output.name = "2020-08-07_profile.csv")
+clean.exo.data(file.name = "exo_profile_ME20200814.xlsx",
+               output.name = "2020-08-14_profile.csv")
+clean.exo.data(file.name = "exo_profile_ME20200902.xlsx",
+               output.name = "2020-09-02_profile.csv",
+               depth.adjustment = 1)
+clean.exo.data(file.name = "exo_profile_ME20200917.xlsx",
+               output.name = "2020-09-17_profile.csv")
+clean.exo.data(file.name = "exo_profile_ME20200924.xlsx",
+               output.name = "2020-09-24_profile.csv")
+clean.exo.data(file.name = "exo_profile_ME20201010_profile.xlsx",
+               output.name = "2020-10-10_profile.csv",
+               pH.sensor.present = FALSE,
+               depth.adjustment = 0.6)
--- code/cleaning_scripts/cleaning_functions.R
+++ code/cleaning_scripts/cleaning_functions.R
@@ -3,7 +3,7 @@
 # output.file.name <- "dataEdited/incubations/HgT/2020_incubations/20210203.csv"
 # all.metadata.2020 <- read.csv("metadata/processedMetadata/incubation_Hg_metadata_2020.csv",
 #                               stringsAsFactors = FALSE)
-# 
+#
 
 
 #### Clean HgT data ####
@@ -12,12 +12,12 @@ clean.HgT.data.from <- function(file.name.input,
                                 metadata.df = all.metadata.2020,
                                 date.of.analysis,
                                 barcodes.to.remove = NULL) {
-  
+
   file.name <- file.name.input
   file.data <- read_xlsx(file.name,
                          sheet = "Summary",
                          skip = 10)
-  
+
   # Select needed columns and rename them.
   file.data.clean <- file.data %>%
     select(1, 3, 4, 5, 6, 10) %>%
@@ -32,30 +32,30 @@ clean.HgT.data.from <- function(file.name.input,
     filter(sampleType == "SAM") %>%
     select(-sampleType) %>%
     mutate(dateAnalyzed_HgT = date.of.analysis)
-  
+
   # Remove unwanted samples by barcode
   if (!is.null(barcodes.to.remove)) {
-    
+
     file.data.clean <- file.data.clean %>%
       filter(!(bottleID %in% barcodes.to.remove))
-    
+
   }
-  
+
   # Remove entries lacking results
   file.data.clean <- file.data.clean %>%
     filter(!is.na(excess_HgT_198_ng.L),
            !is.na(excess_HgT_204_ng.L))
-  
+
   # Round off numbers
   file.data.clean[, c("amb_HgT_ng.L", "excess_HgT_198_ng.L", "excess_HgT_204_ng.L", "excess_HgT_DDL")] <- file.data.clean[, c("amb_HgT_ng.L", "excess_HgT_198_ng.L", "excess_HgT_204_ng.L", "excess_HgT_DDL")] %>%
     mutate_all(as.numeric) %>%
     round(digits = 3)
-  
+
   file.data.clean <- file.data.clean %>%
     mutate(above_DDL_HgT_198 = (excess_HgT_198_ng.L > excess_HgT_DDL)) %>%
     mutate(above_DDL_HgT_204 = (excess_HgT_204_ng.L > excess_HgT_DDL))
-  
-  
+
+
   # Write out data
   write.csv(file.data.clean,
             output.file.name,
@@ -75,12 +75,12 @@ clean.MeHg.data.from <- function(file.name.input,
                                  metadata.df = all.metadata.2020,
                                  date.of.analysis,
                                  barcodes.to.remove = NULL) {
-  
+
   file.name <- file.name.input
   file.data <- read_xlsx(file.name,
                          sheet = "Summary",
                          skip = 6)
-  
+
   # Select needed columns and rename them.
   file.data.clean <- file.data %>%
     select(1, 3, 4, 5, 9) %>%
@@ -92,35 +92,35 @@ clean.MeHg.data.from <- function(file.name.input,
                                  "excess_MeHg_DDL")
   file.data.clean <- file.data.clean %>%
     filter(bottleID %in% metadata.df$bottleID)
-  
+
   # Remove unwanted samples by barcode
   if (!is.null(barcodes.to.remove)) {
-    
+
     file.data.clean <- file.data.clean %>%
       filter(!(bottleID %in% barcodes.to.remove))
-    
+
   }
-  
+
   # Remove spikes
   file.data.clean <- file.data.clean %>%
     filter(!duplicated(bottleID))
-  
+
   # Remove entries lacking results
   file.data.clean <- file.data.clean %>%
     filter(!is.na(excess_MeHg_198_ng.L),
            !is.na(excess_MeHg_204_ng.L))
-  
+
   # Round off numbers
   file.data.clean[, -1] <- file.data.clean[, -1] %>%
     mutate_all(as.numeric) %>%
     round(digits = 3)
-  
+
   file.data.clean <- file.data.clean %>%
     mutate(above_DDL_MeHg_198 = (excess_MeHg_198_ng.L > excess_MeHg_DDL),
            above_DDL_MeHg_204 = (excess_MeHg_204_ng.L > excess_MeHg_DDL)) %>%
     mutate(dateAnalyzed_MeHg = date.of.analysis)
-  
-  
+
+
   # Write out data
   write.csv(file.data.clean,
             output.file.name,
@@ -148,7 +148,7 @@ clean.sulfate.data.from <- function(data.file.name,
                                n_max = 1,
                                col_names = FALSE) %>%
     unlist(use.names = FALSE)
-  
+
   # Read in sulfate data
   sulfate.data <- read_xlsx(data.file.name,
                             sheet = "raw_results",
@@ -159,12 +159,12 @@ clean.sulfate.data.from <- function(data.file.name,
     rename(sulfurID = injectionID,
            rawConcentration = concentration) %>%
     mutate(rawConcentration = as.numeric(rawConcentration))
-  
+
   # Read in analytical prep data
   prep.data <- read_xlsx(data.file.name,
                          sheet = "sample_prep") %>%
     rename(sulfurID = injectionID)
-  
+
   # Combine data and adjust concentration for dilution
   sample.data <- inner_join(processing.metadata,
                             sulfate.data %>% select(sulfurID, rawConcentration)) %>%
@@ -173,13 +173,85 @@ clean.sulfate.data.from <- function(data.file.name,
     mutate(preservativeDilutionFactor = ((mass - tare) / (mass - tare - preservativeVol))) %>%
     mutate(concentration = concentration * preservativeDilutionFactor) %>%
     select(sulfurID, concentration)
-  
-  
-  
+
+
+
   # Write out data
   write.csv(sample.data,
             output.file.name,
             row.names = FALSE,
             quote = FALSE)
-  
+
+}
+
+
+
+#### Clean exo data ####
+clean.exo.data <- function(file.name = file.name.of.interest,
+                           output.name = output.name.of.interest,
+                           pH.sensor.present = TRUE,
+                           depth.adjustment = 0) {
+  # Read in data
+  exo.data <- read_xlsx(paste("dataRaw/exo/",
+                              file.name,
+                              sep = ""),
+                        skip = 24) %>%
+    as.data.frame()
+
+  # Clean up datafile
+  # Read in custom headers that I made (found in sonde_profile_headers.csv)
+  sonde.names <- c("date", "time", "time_fraction", "site_name", "fault_code", "Battery_V",
+                   "Cable_Pwr_V", "Temp_C", "Cond_µS.cm", "SpCond_µS.cm", "Sal_psu",
+                   "nLF_Cond_µS.cm", "TDS_mg.L", "ODO_sat", "ODO_conc", "fDOM_RFU", "fDOM_QSU",
+                   "pH", "pH_mV", "ORP_mV", "ORP_raw_mV", "Turbidity_FNU", "TSS_mg.L",
+                   "Chlorophyll_RFU", "Chlorophyll_µg.L", "BGA-PC_RFU", "BGA-PC_µg.L",
+                   "Press_psi_a", "depth")
+  if (pH.sensor.present == FALSE) {
+    sonde.names <- c("date", "time", "time_fraction", "site_name", "fault_code", "Battery_V",
+                     "Cable_Pwr_V", "Temp_C", "Cond_µS.cm", "SpCond_µS.cm", "Sal_psu",
+                     "nLF_Cond_µS.cm", "TDS_mg.L", "ODO_sat", "ODO_conc", "fDOM_RFU", "fDOM_QSU",
+                     "Turbidity_FNU", "TSS_mg.L", "Chlorophyll_RFU", "Chlorophyll_µg.L",
+                     "BGA-PC_RFU", "BGA-PC_µg.L", "Press_psi_a", "depth")
+  }
+  # Add names, clean the data
+  colnames(exo.data) <- sonde.names
+
+  # Factor in depth adjustment
+  exo.data$depth <- exo.data$depth - depth.adjustment
+
+  # Start at 1 meters
+  exo.data <- exo.data[which(exo.data$depth >= 1), ]
+  # End at maximum depth
+  exo.data <- exo.data[1:which.max(exo.data$depth), ]
+  # Order by depth and time
+  exo.data <- exo.data[order(exo.data$depth, exo.data$time), ]
+  # Round depths to nearest 0.1m and average these measurements
+  exo.data$depth <- round(exo.data$depth,
+                          digits = 1)
+
+  if (pH.sensor.present == TRUE) {
+
+    exo.data <- exo.data %>%
+      select(Temp_C, Cond_µS.cm, SpCond_µS.cm, Sal_psu,
+             ODO_sat, ODO_conc, fDOM_RFU, pH, ORP_mV,
+             Turbidity_FNU, Chlorophyll_RFU, depth)
+  } else {
+    exo.data <- exo.data %>%
+      select(Temp_C, Cond_µS.cm, SpCond_µS.cm, Sal_psu,
+             ODO_sat, ODO_conc, fDOM_RFU, Turbidity_FNU,
+             Chlorophyll_RFU, depth)
+  }
+
+  exo.data <- aggregate(exo.data,
+                        by = list(exo.data$depth),
+                        FUN = mean) %>%
+    select(-Group.1)
+
+  # Write out csv of data
+  write.csv(exo.data,
+            file = paste("dataEdited/exo/",
+                         output.name,
+                         sep = ""),
+            row.names = FALSE)
+
 }
