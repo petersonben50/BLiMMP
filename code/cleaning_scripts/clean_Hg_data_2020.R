8eee4f0c897a22cc2a817f6dfb518f5cb6eda6d6
Benjamin D. Peterson <petersonben50@gmail.com>
2021-04-16 22:49:18 -0500

    Cleaning 2020 water column Hg data

---
+++ code/cleaning_scripts/clean_Hg_data_2020.R
@@ -0,0 +1,82 @@
+# code/cleaning_scripts/clean_Hg_data_2020.R
+
+# This scripts reads in the Hg data that I received from the 
+# USGS Hg team for the 2020 sampling trips and combines it
+# into one data set. The data is then saved as a csv.
+
+#### Always get a clean start ####
+
+rm(list = ls())
+setwd("~/Documents/research/BLiMMP")
+library(dplyr)
+library(lubridate)
+library(tidyr)
+library(readxl)
+
+
+#### Function to clean individual data sheets ####
+
+dataSheet <- "dataRaw/Hg/MENDOTA_results_2020-11-03.csv"
+waterDepth = 24
+output = "dataEdited/Hg/2020/clean_Hg_data_20201103.csv"
+
+clean.Hg.data.for <- function(dataSheet,
+                              waterDepth = 24,
+                              output) {
+  
+  raw.Hg.data <- read.csv(dataSheet,
+                          stringsAsFactors = FALSE)
+  
+  clean.Hg.data <- raw.Hg.data %>%
+    rename(depthOriginal = Depth,
+           concentration = Result.Value,
+           constituent = Constituent,
+           sampleTime = Sample.Time,
+           unit = Unit) %>%
+    mutate(corewater = depthOriginal < 0,
+           depth = depthOriginal + (waterDepth * corewater),
+           sampleDate = mdy(Sample.Date)) %>%
+    select(sampleDate, depth, sampleTime, depthOriginal, concentration,
+           unit, constituent, DDL, corewater)
+  
+  write.csv(clean.Hg.data,
+            output,
+            row.names = FALSE)
+  }
+
+
+
+
+#### Clean individual sheets ####
+
+clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2020-10-09.csv",
+                  waterDepth = 24,
+                  output = "dataEdited/Hg/2020/clean_Hg_data_20201009.csv")
+clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2020-11-03.csv",
+                  waterDepth = 24,
+                  output = "dataEdited/Hg/2020/clean_Hg_data_20201103.csv")
+clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2021-01-19.csv",
+                  waterDepth = 24,
+                  output = "dataEdited/Hg/2020/clean_Hg_data_20210119.csv")
+
+
+
+
+#### Aggregate all cleaned data files for 2020 ####
+
+rm(list = ls())
+
+list.o.files <- list.files(path = "./dataEdited/Hg/2020",
+                           pattern = "clean_Hg_data",
+                           full.names = TRUE)
+Hg.data <- do.call(rbind,
+                   lapply(list.o.files,
+                          function(x) {
+                            read.csv(x,
+                                     stringsAsFactors = FALSE)
+                          }))
+
+
+write.csv(Hg.data,
+          file = "dataEdited/Hg/Hg_data_2020.csv",
+          row.names = FALSE)
