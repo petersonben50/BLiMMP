119c4149c45ab29c500da5d3e76e39bd4161b77a
Benjamin D. Peterson <petersonben50@gmail.com>
2021-05-24 10:56:19 -0500

    Added scripts to clean assemblies, check reports

--- code/assemblies/assembly_processing.md
+++ code/assemblies/assembly_processing.md
@@ -59,3 +59,22 @@ I used SPAdes v3.14.1.
 Because I wanted to do a coassembly and since I hope to use duplicate metagenomes in 2021, I used a script (`assembly_by_group.py`) to run the assemblies using the needed metagenomes.
 As usual, I used these kmers for assembly: 21,33,55,77,99,127.
 I ran it using 24 cores with a memory limit of 1 Tb.
+
+*Clean up assemblies*
+
+I didn't want to include any scaffolds under 1000 bp in length, so I used anvi'o to strip out the sequences lower than 1000 bp in length.
+I also simplified the scaffold names.
+
+
+**Predict ORFs**
+
+I then used a submission script to run the ORF predictions, using the same methods as used for 5M, EG, and HCC studies.
+I used Prodigal (V2.6.3) on the `meta` mode and saved out fna, faa, and gff files.
+
+
+**Get assembly stats**
+
+Finally, I took a look at the assemblies.
+I used the `abyss-fac.pl` script, written by [Shaun Jackman](sjackman@bcgsc.ca), to check the length and N50 values of the assemblies.
+I aggregated those stats.
+I then also counted the number of predicted ORFs for each assembly.
--- code/assemblies/assembly_processing.sh
+++ code/assemblies/assembly_processing.sh
@@ -235,3 +235,117 @@ do
     echo $assembly "already assembled"
   fi
 done
+
+
+######################
+# Clean up assemblies
+######################
+screen -S HCC_clean_metagenome_assembly
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate anvio6.2
+PYTHONPATH=''
+cd ~/BLiMMP/dataEdited/assemblies
+mkdir scaffolds
+mkdir scaffolds/renaming_reports
+
+cat ~/BLiMMP/metadata/assembly_list.txt | while read assembly
+do
+  if [ -e assembly_files/$assembly/scaffolds.fasta ]; then
+    echo $assembly "is done assembling. Let's clean it."
+    if [ -e scaffolds/$assembly\_assembly.fna ]; then
+      echo "Dude, you already cleaned the assembly for" $assembly". Relax."
+    else
+      echo "Cleaning the assembly for" $assembly
+      anvi-script-reformat-fasta assembly_files/$assembly/scaffolds.fasta \
+                                  -o scaffolds/$assembly\_assembly.fna \
+                                  -l 1000 \
+                                  --simplify-names \
+                                  --prefix $assembly \
+                                  --report-file scaffolds/renaming_reports/$assembly\_report_file.txt
+    fi
+  else
+    echo "Yo, you gotta go back and assemble" $assembly
+  fi
+done
+conda deactivate
+
+
+############################################
+############################################
+# Predict ORFs
+############################################
+############################################
+
+# Get set up
+#cd /home/GLBRCORG/bpeterson26/BLiMMP/
+#cp /home/GLBRCORG/bpeterson26/HellsCanyon/code/generalUse/cleanFASTA.py code/cleanFASTA.py
+#mkdir reports
+#cd reports
+#mkdir outs errs logs
+
+
+mkdir /home/GLBRCORG/bpeterson26/BLiMMP/dataEdited/assemblies/ORFs
+chmod +x /home/GLBRCORG/bpeterson26/BLiMMP/code/ORF_prediction_assemblies.sh
+condor_submit /home/GLBRCORG/bpeterson26/BLiMMP/code/ORF_prediction_assemblies.sub
+
+
+
+
+
+############################################
+############################################
+# Get assembly stats
+############################################
+############################################
+
+screen -S BLI_assembly_stats
+cd ~/BLiMMP/dataEdited/assemblies
+mkdir reports
+code=~/BLiMMP/code
+cp ~/HellsCanyon/code/assembly_processing/abyss-fac.pl $code/
+
+# Use abyss-fac to check stats
+cat ~/BLiMMP/metadata/assembly_list.txt | while read assembly
+do
+  if [ -e scaffolds/$assembly\_assembly.fna ]; then
+    echo $assembly "has been cleaned. Let's get some stats on it."
+    if [ ! -e reports/$assembly\_report.txt ]; then
+      perl $code/abyss-fac.pl scaffolds/$assembly\_assembly.fna \
+          > reports/$assembly\_report.txt
+    else
+      echo "Already checked" $assembly
+    fi
+  else
+    echo $assembly "has not been cleaned. Do that first."
+  fi
+done
+
+# Aggregate stats
+cd ~/BLiMMP/dataEdited/assemblies/reports
+echo -e "n\tn:200\tL50\tmin\tN80\tN50\tN20\tmax\tsum\tassemblyID" > all_assemblies_stats.txt
+for file in *report.txt
+do
+  tail -n +2 $file >> all_assemblies_stats.txt
+done
+# rm BLI*_report.txt
+
+# Clean up the report file
+cat ~/BLiMMP/metadata/assembly_list.txt | while read assembly
+do
+  sed "s/scaffolds\/$assembly\_assembly.fna/$assembly/" all_assemblies_stats.txt \
+    > all_assemblies_stats.txt_edited
+  mv -f all_assemblies_stats.txt_edited all_assemblies_stats.txt
+done
+
+
+# Count ORFs
+cd ~/BLiMMP/dataEdited/assemblies
+echo -e 'assemblyID\tORF_count' > reports/ORF_counts.tsv
+cat ~/BLiMMP/metadata/assembly_list.txt | while read assembly
+do
+  if [ -e ORFs/$assembly.faa ]; then
+    echo "Count ORFs in" $assembly
+    ORF_count=$(grep '>' ORFs/$assembly.faa | wc -l)
+    echo -e "$assembly\t$ORF_count" >> reports/ORF_counts.tsv
+  fi
+done
