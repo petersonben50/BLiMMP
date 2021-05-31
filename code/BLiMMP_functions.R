497a01036538ad2614465873a66199dc6abec237
Benjamin D. Peterson <petersonben50@gmail.com>
2021-05-31 14:20:09 -0500

    Added scripts to plot BGC profiles

--- code/BLiMMP_functions.R
+++ code/BLiMMP_functions.R
@@ -1,6 +1,369 @@
 
-#### Function to plot change in Me198Hg for each depth on a given trip ####
 
+
+
+
+
+
+#### Function to plot exo data ####
+
+plot.exo.data <- function(date.of.sampling,
+                          points.of.sampling = NULL) {
+  # Read in exo data
+  exo.data.file.name <- paste("dataEdited/exo/",
+                              date.of.sampling,
+                              "_profile.csv",
+                              sep = "")
+  exo.data <- read.csv(exo.data.file.name,
+                       stringsAsFactors = FALSE)
+  # Generate plot
+  par(mar=c(9,3,2.5,1), mgp=c(1.5,0.4,0), tck=-0.008)
+  
+  # Plot the temp points and set up graph
+  plot(x = (exo.data$Temp_C-5)/2.5,
+       y = -exo.data$depth,
+       type = "l",
+       lwd = 3,
+       xaxt = "n",
+       xlab = "",
+       xlim = c(0, 10),
+       yaxt = "n",
+       ylab = "",
+       ylim = c(-25, 0),
+       col = cb.translator["blue"])
+  # Plot DO
+  DO.fudge.factor <- 15
+  lines(x = exo.data$ODO_sat/DO.fudge.factor,
+        y = -exo.data$depth,
+        col = cb.translator["black"],
+        lwd = 3,
+        lty = 4)
+  # Plot the turbidity values
+  turb.fudge.factor <- 0.5
+  lines(x = exo.data$Turbidity_FNU/turb.fudge.factor,
+        y = -exo.data$depth,
+        col = cb.translator["reddishpurple"],
+        lwd = 4,
+        lty = 3)
+  # Add y-axis with depth measurements
+  axis(2,
+       at = seq(0, -25, by = -5),
+       labels = seq(0, 25, by = 5))
+  # Add axis for temperature
+  axis(1,
+       at = seq(0, 10, by = 2),
+       labels = seq(0, 10, by = 2)*3)
+  # Add axis for DO values
+  axis(1,
+       at = seq(0, 10, by = 2),
+       labels = seq(0, 10, by = 2)*DO.fudge.factor,
+       line = 3,
+       par(mgp = c(3, 0.5, 0)))
+  # Add axis for turbidity values
+  axis(1,
+       at = seq(0, 10, by = 2),
+       labels = seq(0, 10, by = 2)*turb.fudge.factor,
+       line = 6,
+       par(mgp = c(3, 0.5, 0)))
+  
+  
+  # Add titles for depth
+  title(xlab = "Temp (C)",
+        ylab = "Depth (m)",
+        line = 1.5)
+  # Add title for dissolved oxygen
+  title(xlab = "ODO (% sat)",
+        line = 4.5)
+  # Add title for turbidity
+  title(xlab = "Turbidity (FNU)",
+        line = 7.5)
+  title(main = date.of.sampling)
+  
+  # OPTIONAL: Add in sampling points
+  if (!is.null(points.of.sampling)) {
+    points(x = rep(9, length(points.of.sampling)),
+           y = -points.of.sampling,
+           pch = 16,
+           cex = 2,
+           col = cb.translator["bluishgreen"])
+    legend("topright",
+           legend = c("Temperature", "DO", "Turbidity", "Sampling Points"),
+           col = c(cb.translator["blue"], cb.translator["black"], cb.translator["reddishpurple"], cb.translator["bluishgreen"]),
+           lwd = c(3, 3, 3, NA),
+           lty = c(1, 4, 3, NA),
+           pt.cex = c(NA, NA, NA, 1.75),
+           pch = c(NA, NA, NA, 16),
+           bg = "white")
+  } else {
+    legend("topright",
+           legend = c("Temperature", "DO", "Turbidity"),
+           col = c(cb.translator["blue"], cb.translator["black"], cb.translator["reddishpurple"]),
+           lwd = 3,
+           lty = c(1, 4, 3, NA),
+           pch = NA,
+           bg = "white")
+  }
+}
+
+
+
+#### Function to plot geochem data ####
+plot.redox.profile <- function(trip,
+                               sulfide.data.location,
+                               sulfate.data.location,
+                               ICP.data.name,
+                               plot.fe = "yes") {
+  
+  # Read in sulfide data
+  sulfide.data.date <- read.csv(sulfide.data.location,
+                                stringsAsFactors = FALSE) %>%
+    filter(tripID == trip) %>%
+    mutate(depth = as.numeric(depth))
+  
+  # Read in sulfate data
+  sulfate.data.date <- read.csv(sulfate.data.location,
+                                stringsAsFactors = FALSE) %>%
+    filter(tripID == trip) %>%
+    mutate(depth = as.numeric(depth))
+  
+  # Read in ICP data
+
+  Mn.data.date <- readRDS(ICP.data.name) %>%
+    filter(tripID == trip,
+           constituent == "dissolved_Mn_ppb") %>%
+    mutate(diss_conc_uM = concentration/ 54.938)
+  Mn.part.data.date <- readRDS(ICP.data.name) %>%
+    filter(tripID == trip,
+           constituent == "particulate_Mn_ppb") %>%
+    mutate(part_conc_uM = concentration/ 54.938)
+
+  Fe.data.date <- readRDS(ICP.data.name) %>%
+    filter(tripID == trip,
+           constituent == "dissolved_Fe_ppb") %>%
+    mutate(diss_conc_uM = concentration/ 55.85)
+  Fe.part.data.date <- readRDS(ICP.data.name) %>%
+    filter(tripID == trip,
+           constituent == "particulate_Fe_ppb") %>%
+    mutate(diss_conc_uM = concentration/ 55.85)
+  
+
+  
+  # Generate plot, add sulfide data points
+  sulfide.adjustment.for.plotting <- 0.1
+  plot(x = sulfide.data.date$S_conc*sulfide.adjustment.for.plotting,
+       y = -sulfide.data.date$depth,
+       ylim = c(-25, 0),
+       xlim = c(-1, 23),
+       pch = 16,
+       cex = 1.8,
+       col = cb.translator["blue"],
+       xaxt = "n",
+       xlab = "",
+       yaxt = "n",
+       ylab = "")
+  lines(x = sulfide.data.date$S_conc*sulfide.adjustment.for.plotting,
+        y = -sulfide.data.date$depth,
+        col = cb.translator["blue"])
+
+  # Add sulfate data points
+  sulfate.adjustment.for.plotting <- 0.1
+  points(x = sulfate.data.date$concentration/96.06*1000*sulfate.adjustment.for.plotting,
+         y = -as.numeric(sulfate.data.date$depth),
+         pch = 1,
+         cex = 1.5,
+         col = cb.translator["blue"])
+  lines(x = sulfate.data.date$concentration/96.06*1000*sulfate.adjustment.for.plotting,
+        y = -as.numeric(sulfate.data.date$depth),
+        col = cb.translator["blue"])
+  
+  # Add dissolved Mn data points
+  Mn.adjustment.for.plotting <- 3
+  points(x = Mn.data.date$diss_conc_uM*Mn.adjustment.for.plotting,
+         y = -as.numeric(Mn.data.date$depth),
+         pch = 18,
+         cex = 2,
+         col = cb.translator["orange"])
+  lines(x = Mn.data.date$diss_conc_uM*Mn.adjustment.for.plotting,
+        y = -as.numeric(Mn.data.date$depth),
+        col = cb.translator["orange"])
+  
+  # Add particulate Mn data points
+  points(x = Mn.part.data.date$part_conc_uM*Mn.adjustment.for.plotting,
+         y = -as.numeric(Mn.part.data.date$depth),
+         pch = 5,
+         cex = 1.5,
+         col = cb.translator["orange"])
+  lines(x = Mn.part.data.date$part_conc_uM*Mn.adjustment.for.plotting,
+        y = -as.numeric(Mn.part.data.date$depth),
+        col = cb.translator["orange"])
+
+  # # Add dissolved Fe data points
+  # Fe.adjustment.for.plotting <- 1
+  # points(x = Fe.part.data.date$diss_conc_uM*Fe.adjustment.for.plotting,
+  #        y = -as.numeric(Fe.part.data.date$depth),
+  #        pch = 15,
+  #        cex = 1.5,
+  #        col = cb.translator["reddishpurple"])
+  # lines(x = Fe.part.data.date$diss_conc_uM*Fe.adjustment.for.plotting,
+  #       y = -as.numeric(Fe.part.data.date$depth),
+  #       col = cb.translator["reddishpurple"])
+  # 
+  # # Add particulate Fe data points
+  # Fe.adjustment.for.plotting <- 1
+  # points(x = Fe.data.date$diss_conc_uM*Fe.adjustment.for.plotting,
+  #        y = -as.numeric(Fe.data.date$depth),
+  #        pch = 0,
+  #        cex = 1.5,
+  #        col = cb.translator["reddishpurple"])
+  # lines(x = Fe.data.date$diss_conc_uM*Fe.adjustment.for.plotting,
+  #       y = -as.numeric(Fe.data.date$depth),
+  #       col = cb.translator["reddishpurple"])
+  
+  
+  # Add y-axis with depth measurements
+  axis(2,
+       at = seq(0, -25, by = -5),
+       labels = seq(0, 25, by = 5))
+  
+  # Add axis for sulfide
+  axis(1,
+       at = seq(0, 20, by = 4),
+       labels = seq(0, 20, by = 4)/sulfide.adjustment.for.plotting)
+  
+  # Add axis for Mn
+  axis(1,
+       line = 3,
+       at = seq(0, 21, by = 3),
+       labels = seq(0, 21, by = 3)/Mn.adjustment.for.plotting)
+
+  # Add label for depth
+  title(ylab = "Depth (m)",
+        line = 1.5)
+  
+  # Add label for sulfide
+  title(xlab = "Sulfide (uM)",
+        line = 1.5)
+  
+  # Add label for Mn
+  title(xlab = "Mn (µM)",
+        line = 4.5)
+  
+  # Add legend
+  legend("topright",
+         legend = c("Sulfide",
+                    "Dissolved Mn",
+                    "Particulate Mn"),
+         col = c(cb.translator["blue"],
+                 cb.translator["orange"],
+                 cb.translator["orange"]),
+         pch = c(16, 18, 5),
+         pt.cex = c(1.75, 1.75, 1.25),
+         bty = "n")
+}
+
+
+#### Function to plot Hg data ####
+plot.Hg.profile <- function(tripDates,
+                            Hg.data.location) {
+  
+  # Read in Hg data
+  Hg.data.date <- read.csv(Hg.data.location) %>%
+    filter(sampleDate %in% tripDates)
+  
+  # Split out values
+  FMHG.values <- Hg.data.date %>%
+    filter(constituent == "FMHG_NG.L") %>%
+    arrange(depth) %>%
+    select(depth, concentration)
+  FiHG.values <- Hg.data.date %>%
+    filter(constituent == "FiHg_NG.L") %>%
+    arrange(depth) %>%
+    select(depth, concentration)
+  
+
+  percent.MeHg <- Hg.data.date %>%
+    spread(key = constituent,
+           value = concentration) %>%
+    mutate(perMeHg = FMHG_NG.L / FTHG_NG.L) %>%
+    arrange(depth)
+
+  # Plot MeHg values
+  plot(x = FMHG.values$concentration,
+       y = FMHG.values$depth,
+       ylim = c(25, 0),
+       xlim = c(0, 1.5),
+       pch = 17,
+       cex = 1.5,
+       col = cb.translator["vermillion"],
+       xaxt = "n",
+       xlab = "",
+       yaxt = "n",
+       ylab = "")
+  lines(FMHG.values$concentration,
+        FMHG.values$depth,
+        col = cb.translator["vermillion"])
+  
+  # Plot iHg values
+  points(x = FiHG.values$concentration,
+         y = FiHG.values$depth,
+         pch = 18,
+         cex = 1.5,
+         col = cb.translator["bluishgreen"])
+  lines(x = FiHG.values$concentration,
+        y = FiHG.values$depth,
+        col = cb.translator["bluishgreen"])
+  
+  #### Plot percent MeHg values ####
+  points(x = percent.MeHg$perMeHg*1.5,
+         y = percent.MeHg$depth,
+         pch = 16,
+         cex = 1.5,
+         col = cb.translator["black"])
+  lines(x = percent.MeHg$perMeHg*1.5,
+        y = percent.MeHg$depth,
+        col = cb.translator["black"])
+  
+  # Add y-axis with depth measurements
+  axis(2,
+       at = seq(0, 25, by = 5),
+       labels = seq(0, 25, by = 5))
+    # Add axis for Hg
+  axis(1,
+       at = seq(0, 1.5, by = 0.3),
+       labels = seq(0, 1.5, by = 0.3))
+  axis(1,
+       line = 3,
+       at = seq(0, 1.5, by = 0.375),
+       labels = seq(0, 1.5, by = 0.375)/1.5)
+  
+  #### Add labels ####
+  # Add label for percent MeHg
+  title(xlab = "Percent MeHg",
+        line = 4.5)
+  # Add label for depth
+  title(ylab = "Depth (m)",
+        line = 1.5)
+  # Add label for sulfide
+  title(xlab = "Hg (ng/L)",
+        line = 1.5)
+  
+  
+  # Add legend
+  legend("topright",
+         legend = c("iHg",
+                    "MeHg",
+                    "% MeHg"),
+         col = c(cb.translator["bluishgreen"],
+                 cb.translator["vermillion"],
+                 cb.translator["black"]),
+         pch = c(18, 17, 16),
+         pt.cex = c(1.75, 1.5, 1.75),
+         bty = "n")
+}
+
+
+
+#### Function to plot change in Me198Hg for each depth on a given trip ####
 plot.Hg.time.course.depth <- function(selected.depth,
                                       trip_data,
                                       parameter.of.interest,
@@ -116,16 +479,6 @@ plot.Hg.time.course.depth <- function(selected.depth,
   }
   
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
