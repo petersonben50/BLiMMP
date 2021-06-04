036b62961d090a916484997bed8f3bebef2488d1
Benjamin D. Peterson <petersonben50@gmail.com>
2021-06-04 11:13:48 -0500

    Scripts to get clean depths for bins, and some cleanup

--- code/binning/binning_workflow.sh
+++ code/binning/binning_workflow.sh
@@ -870,16 +870,6 @@ trimal -in putative_MoORs_ref_1.afa \
         -gt 0.5
 FastTree putative_MoORs_ref_1_masked.afa > putative_MoORs_1.tree
 
-          raxml=/opt/bifxapps/raxml-8.2.11/raxmlHPC-PTHREADS
-          $raxml -f a \
-                  -p 283976 \
-                  -m PROTGAMMAAUTO \
-                  -N autoMRE \
-                  -x 2381 \
-                  -T 20 \
-                  -s putative_MoORs_ref_1_masked.afa \
-                  -n hgcA
-
 
           ####################################################
           ####################################################
