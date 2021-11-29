0e2da993f1970416b9357b9f7309c5ac284b0254
Benjamin D. Peterson <petersonben50@gmail.com>
2021-11-29 15:09:02 -0600

    Updates to script cleaning all Hg data

--- code/cleaning_scripts/Hg_data_cleaning.R
+++ code/cleaning_scripts/Hg_data_cleaning.R
@@ -1,11 +1,16 @@
-# code/cleaning_scripts/clean_Hg_data_2020.R
+#### code/cleaning_scripts/Hg_data_cleaning.R ####
 
-# This scripts reads in the Hg data that I received from the 
-# USGS Hg team for the 2020 sampling trips and combines it
-# into one data set. The data is then saved as a csv.
+# This script used to read in the Hg data that I received from the 
+# USGS Hg team for the 2020 sampling trips and combine it
+# into one data set. Now I've changed it, I downloaded all the Hg
+# data related to my project from Merlins into one big file. Everything
+# from the Lake Mendota project after 2020-07-01. This is saved here:
+# dataRaw/Hg/USGS_results_2021-11-09.xlsx
+
+# This file includes the incubation results as well.
+
 
 #### Always get a clean start ####
-
 rm(list = ls())
 setwd("~/Documents/research/BLiMMP")
 library(dplyr)
@@ -14,94 +19,70 @@ library(tidyr)
 library(readxl)
 
 
-#### Function to clean individual data sheets ####
-
-dataSheet <- "dataRaw/Hg/MENDOTA_results_2020-11-03.csv"
-waterDepth = 24
-output = "dataEdited/Hg/2020/clean_Hg_data_20201103.csv"
-
-clean.Hg.data.for <- function(dataSheet,
-                              waterDepth = 24,
-                              output) {
-  
-  raw.Hg.data <- read.csv(dataSheet,
-                          stringsAsFactors = FALSE)
-  
-  clean.Hg.data <- raw.Hg.data %>%
-    rename(depthOriginal = Depth,
-           concentration = Result.Value,
-           constituent = Constituent,
-           sampleTime = Sample.Time,
-           unit = Unit) %>%
-    mutate(corewater = depthOriginal < 0,
-           depth = depthOriginal + (waterDepth * corewater),
-           sampleDate = mdy(Sample.Date)) %>%
-    select(sampleDate, depth, sampleTime, depthOriginal, concentration,
-           unit, constituent, DDL, corewater)
-  
-  write.csv(clean.Hg.data,
-            output,
-            row.names = FALSE)
-  }
+#### Set variables ####
+dataSheet <- "dataRaw/Hg/USGS_results_2021-11-09.xlsx"
+waterDepth <- 24
+output <- "dataEdited/Hg/Hg_data_clean.csv"
 
 
+#### Read in data ####
+Hg.data.raw <- read_xlsx(dataSheet) %>%
+  rename(bottleID = bottle,
+         depthOriginal = depth,
+         concentration = result_value,
+         sampleTime = sample_time) %>%
+  mutate(corewater = depthOriginal < 0,
+         depth = depthOriginal + (waterDepth * corewater),
+         sampleDate = ymd(sample_date)) %>%
+  select(sampleDate, depth, sampleTime, depthOriginal, concentration,
+         unit, constituent, detection_flag, corewater)
 
 
-#### Clean individual sheets ####
+#### Keep only water column data ####
+# No corewater or incubation data
+Hg.data <- Hg.data.raw %>%
+  filter(!(constituent %in% c("UMHG", "UTHG")),
+         !(sampleDate %in% c(as.Date("2020-08-14"),
+                             as.Date("2020-09-24"))))
+rm(Hg.data.raw)
 
-clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2020-10-09.csv",
-                  waterDepth = 24,
-                  output = "dataEdited/Hg/2020/clean_Hg_data_20201009.csv")
-clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2020-11-03.csv",
-                  waterDepth = 24,
-                  output = "dataEdited/Hg/2020/clean_Hg_data_20201103.csv")
-clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2021-01-19.csv",
-                  waterDepth = 24,
-                  output = "dataEdited/Hg/2020/clean_Hg_data_20210119.csv")
-clean.Hg.data.for(dataSheet = "dataRaw/Hg/MENDOTA_results_2021-05-06_editedHeaders_noIncubationSamples.csv",
-                  waterDepth = 24,
-                  output = "dataEdited/Hg/2020/clean_Hg_data_20210506.csv")
-
-
-
-rm(list = ls())
-
-
-#### Aggregate all cleaned data files for 2020 ####
-list.o.files <- list.files(path = "./dataEdited/Hg/2020",
-                           pattern = "clean_Hg_data",
-                           full.names = TRUE)
-Hg.data <- do.call(rbind,
-                   lapply(list.o.files,
-                          function(x) {
-                            read.csv(x,
-                                     stringsAsFactors = FALSE)
-                          }))
-
-
-#### Prep inorganic Hg data ####
-clean.Hg.data <- Hg.data %>%
+#### Prep inorganic data ####
+Hg.data <- Hg.data %>%
   mutate(constituent = paste(constituent, "_", unit,
                              sep = "") %>%
            gsub("/", ".", .)) %>%
-  group_by(sampleDate, depth, constituent, corewater) %>%
-  summarize(concentration = mean(concentration)) %>%
+  select(-unit)
+
+iHg.data <- Hg.data %>%
+  select(-detection_flag) %>%
+  filter(constituent %in% c("FTHG_NG.L", "FMHG_NG.L",
+                            "PTHG_NG.L", "PMHG_NG.L")) %>%
   spread(key = constituent,
          value = concentration) %>%
   mutate(FiHg_NG.L = FTHG_NG.L - FMHG_NG.L,
          PiHg_NG.L = PTHG_NG.L - PMHG_NG.L) %>%
-  gather(key = constituent,
+    gather(key = constituent,
          value = concentration,
-         -c(1:3)) %>%
-  filter(!is.na(concentration))
-  
+         c(6:11)) %>%
+  filter(constituent %in% c("FiHg_NG.L",
+                            "PiHg_NG.L")) %>%
+  mutate(detection_flag = "NA") %>%
+  select(sampleDate, depth, sampleTime, depthOriginal,
+         concentration, constituent, detection_flag, corewater)
+
+Hg.data <- rbind(Hg.data,
+                 iHg.data)
+rm(iHg.data)
 
 
-
+#### Fix surface samples from 2021-09-10/11 ####
+# We have two surface samples listed... not sure why,
+# and not sure why one is listed on 2021-09-11. Let's 
+# just remove that one, we only need one here.
+Hg.data <- Hg.data %>%
+  filter(!(sampleDate == "2021-09-11" & depth == 0.0 & sampleTime == "1300"))
 
 #### Read out data ####
-write.csv(clean.Hg.data,
-          file = "dataEdited/Hg/Hg_data_2020.csv",
+write.csv(Hg.data,
+          file = output,
           row.names = FALSE)
-
-
