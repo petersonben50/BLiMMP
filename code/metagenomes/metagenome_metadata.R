23a2b434d6d0010f04ecf62ed47de8334d2e53fb
Benjamin D. Peterson <petersonben50@gmail.com>
2022-09-03 23:44:58 -0700

    Simplified to only look at metagenome metadata

--- code/metagenomes/metagenome_metadata.R
+++ code/metagenomes/metagenome_metadata.R
@@ -1,59 +1,39 @@
-#### code/metagenome_metadata.R ####
+#### code/metagenomes/metagenome_metadata.R ####
 # Benjamin D. Peterson
 
-# This file contains the code to generate nice tables with
-# information on the metagenome sequencing and assembly.
-# Also generated R objects for use in other analyses.
+# This file contains the code to generate the combined
+# metadata for the metagenome sequencing and assembly.
 
 
 #### Always start with a clean slate, ya bum ####
 rm(list = ls())
 setwd("/Users/benjaminpeterson/Documents/research/BLiMMP/")
 library(dplyr)
-library(gridExtra)
 library(readxl)
 
 
-#### Generate mapping key ####
-MG.list <- read.table("metadata/lists/metagenome_list.txt",
-                      header = FALSE,
-                      col.names = c("metagenomeID", "samplingYear"))
-assembly.list <- read.table("metadata/lists/assembly_list.txt",
-                            header = FALSE,
-                            col.names = c("assemblyID", "samplingYear"))
-mapping.key <- full_join(MG.list,
-                         assembly.list) %>%
-  select(metagenomeID, assemblyID, samplingYear)
-
-write.table(mapping.key,
-            "metadata/lists/mapping_key.tsv",
-            col.names = FALSE,
-            row.names = FALSE,
-            sep = "\t",
-            quote = FALSE)
-
-
-
-
 #### Generate table of MG sample site information ####
-
 # Read in filter metadata
-all.data <- read_xlsx("metadata/NA_IDs.xlsx") %>%
+all.data <- read_xlsx("metadata/raw_metadata/NA_IDs.xlsx") %>%
   select(filterID, sampleID, volumeFiltered) %>%
   # Read in extraction metadata
   right_join(read_xlsx("dataEdited/dnaExtractions/DNA_extractions.xlsx") %>%
                filter(filterID != "NA") %>%
                select(filterID, extractionID)) %>%
   # Read in sample metadata
-  left_join(read_xlsx("metadata/2_sample_IDs.xlsx") %>%
+  left_join(read_xlsx("metadata/raw_metadata/2_sample_IDs.xlsx") %>%
               select(sampleID, tripID, depth)) %>%
   # Read in trip metadata
-  left_join(read_xlsx("metadata/1_trip_IDs.xlsx") %>%
+  left_join(read_xlsx("metadata/raw_metadata/1_trip_IDs.xlsx") %>%
               select(tripID, startDate)) %>%
   # Read in metagenome metadata
-  left_join(read_xlsx("dataEdited/dnaSequencing/samplePrep/KMBP010_dilutions.xlsx") %>%
-              select(extractionID, metagenomeID)) %>%
-  select(metagenomeID, sampleID, startDate, depth, volumeFiltered)
+  right_join(rbind(read_xlsx("dataEdited/dnaSequencing/2020/samplePrep/KMBP010_dilutions.xlsx") %>%
+                     select(extractionID, metagenomeID),
+                   read_xlsx("dataEdited/dnaSequencing/2021/samplePrep/BLI21_MG_dilutions.xlsx") %>%
+                     rename(metagenomeID = `original metagenomeID`) %>%
+                     select(extractionID, metagenomeID))) %>%
+  select(metagenomeID, sampleID, startDate, depth, volumeFiltered) %>%
+  as.data.frame()
 
 
 #### Save out metadata for analysis ####
