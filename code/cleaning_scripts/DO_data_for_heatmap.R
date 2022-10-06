04e0e82a53d65ed358f4efab7dc9fed004b756db
Benjamin D. Peterson <petersonben50@gmail.com>
2022-10-06 16:59:26 -0700

    Updated cleaning script to include 2021 DO data

--- code/cleaning_scripts/DO_data_for_heatmap.R
+++ code/cleaning_scripts/DO_data_for_heatmap.R
@@ -1,9 +1,13 @@
-#### code/cleaning_scripts/clean_exo_data.R ####
+#### code/cleaning_scripts/DO_data_for_heatmap.R ####
 # Benjamin D. Peterson
 
-# This scripts reads in the sonde data collected on the Exo
-# sonde. The data was collected as part of the BLiMMP project.
-# These samples were collected from Lake Mendota
+# This script prepares a set of DO data to generate
+# a heatmap of DO over 2020 and 2021 summers.
+# For 2020, this data was collected by Mark Gahler.
+# In 2021, I used the Mendota Microbial Observatory
+# data through August 2nd, when the sonde broke.
+# I'm really just interested in the onset of anoxia,
+# so that will be sufficient for my needs for now.
 
 
 #### This is your mess, you need to clean it up ####
@@ -16,26 +20,22 @@ library(readxl)
 
 
 #### Read in and combine data ####
-sonde.files <- list.files("dataRaw/MeMO_exo",
+sonde.files <- list.files("dataRaw/DO_profiles",
                           pattern = "_ME_profile.csv")
 
 for (position.of.interest in 1:length(sonde.files)) {
   file.of.interest <- sonde.files[[position.of.interest]]
-  # date.of.interest <- file.of.interest %>%
-  #   strsplit("_") %>% sapply("[", 1) %>%
-  #   ymd() %>% as.character()
-  # 
   if (file.of.interest == sonde.files[1]) {
-    exo.data <- read.csv(paste("dataRaw/MeMO_exo/",
+    exo.data <- read.csv(paste("dataRaw/DO_profiles/",
                                file.of.interest,
                                sep = "")) %>%
-      select(sampledate, depth, wtemp, do_raw, turb_fnu)
+      select(sampledate, depth, do_raw)
   } else {
     exo.data <- rbind(exo.data,
-                      read.csv(paste("dataRaw/MeMO_exo/",
+                      read.csv(paste("dataRaw/DO_profiles/",
                                      file.of.interest,
                                      sep = "")) %>%
-                        select(sampledate, depth, wtemp, do_raw, turb_fnu))
+                        select(sampledate, depth, do_raw))
   }
 }
 
@@ -53,9 +53,56 @@ exo.data$do_raw[which(exo.data$do_raw < 0)] <- 0
 
 
 #### Only keep the values down to 20 m ####
-exo.data <- exo.data %>%
+exo.data.2020 <- exo.data %>%
   filter(depth <= 20)
 
+rm(exo.data,
+   file.of.interest,
+   position.of.interest,
+   sonde.files)
+
+
+
+
+
+#### Read in and combine MeMO data from 2021 ####
+sonde.files <- list.files("dataRaw/DO_profiles/2021",
+                          pattern = "YSI_MO_")
+
+for (position.of.interest in 1:length(sonde.files)) {
+  file.of.interest <- sonde.files[[position.of.interest]]
+  if (file.of.interest == sonde.files[1]) {
+    MMO.data <- read.csv(paste("dataRaw/DO_profiles/2021/",
+                               file.of.interest,
+                               sep = "")) %>%
+      mutate(sampleDate = mdy_hms(Timestamp) %>% as.Date(),
+             depth = as.numeric(Folder),
+             do_raw = Dissolved.Oxygen..mg.L.) %>%
+      select(sampleDate, depth, do_raw)
+  } else {
+    MMO.data <- rbind(MMO.data,
+                      read.csv(paste("dataRaw/DO_profiles/2021/",
+                                     file.of.interest,
+                                     sep = "")) %>%
+                        mutate(sampleDate = mdy_hms(Timestamp) %>% as.Date(),
+                               depth = as.numeric(Folder),
+                               do_raw = Dissolved.Oxygen..mg.L.) %>%
+                        select(sampleDate, depth, do_raw))
+  }
+}
+
+
+#### Remove all data after 2021-08-02 ####
+# The sonde crapped out after this date
+MMO.data <- MMO.data %>%
+  filter(sampleDate <= as.Date("2021-08-02"))
+
+
+
+#### All data ####
+all.data <- rbind(MMO.data,
+                  exo.data.2020)
+
 #### Save out data ####
-saveRDS(exo.data,
-        "dataEdited/MeMO_exo/MeMO_exo_data.rds")
+saveRDS(all.data,
+        "dataEdited/DO_profiles/DO_profiles_data.rds")
