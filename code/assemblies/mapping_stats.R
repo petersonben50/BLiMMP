139f016d110608d48adb3e8fd2002ed6597f5a3d
Benjamin D. Peterson <petersonben50@gmail.com>
2023-02-02 23:15:57 -0800

    changed location of output

--- code/assemblies/mapping_stats.R
+++ code/assemblies/mapping_stats.R
@@ -32,7 +32,7 @@ mapping.key <- full_join(metagenomes,
                          assemblies) %>%
   select(-year)
 write.table(mapping.key,
-            file = "dataEdited/assemblies/reports/mapping_key.tsv",
+            file = "metadata/lists/mapping_key.tsv",
             quote = FALSE,
             sep = "\t",
             row.names = FALSE,
