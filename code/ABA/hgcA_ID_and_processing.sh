c6397c84754ee64b9b77e330e32269b3d8f9b703
Benjamin D. Peterson <petersonben50@gmail.com>
2024-01-02 23:33:57 -0800

    started on gene neighborhood analysis

--- code/ABA/hgcA_ID_and_processing.sh
+++ code/ABA/hgcA_ID_and_processing.sh
@@ -94,21 +94,23 @@ python $HomeBio/HG_hgcA_AutoClass.py --fasta_file /home/GLBRCORG/bpeterson26/BLi
 # First pull out hgcA+ scaffolds
 ######################
 screen -S BLI_hgcA_context
-cd ~/BLiMMP/dataEdited/hgcA_analysis
+cd /home/GLBRCORG/bpeterson26/BLiMMP/dataEdited/ABA/hgcA
 # rm -r scaffolds
-mkdir scaffolds
+mkdir working_directory/scaffolds
+# Get list of gene names:
+grep '>' hgcA.faa | sed 's/>//' | awk -F ' ' '{print $1}' > hgcA_genes.txt
 
 # Pull out scaffold FNA seqences and GFF entries
-rm -f scaffolds/hgcA_scaffolds.fna scaffolds/hgcA_scaffolds.gff
-awk -F '_' '{ print $1"_"$2"_"$3 }' identification/hgcA_good.txt | while read scaffold
+rm -f working_directory/scaffolds/hgcA_scaffolds.fna working_directory/scaffolds/hgcA_scaffolds.gff
+awk -F '_' '{ print $1"_"$2"_"$3 }' hgcA_genes.txt | while read scaffold
 do
   assemblyName=$(echo $scaffold | awk -F '_' '{ print $1"_"$2 }')
   echo "Pulling out" $scaffold "from" $assemblyName
-  grep -A 1 $scaffold\$ ~/BLiMMP/dataEdited/assemblies/scaffolds/$assemblyName\_assembly.fna \
-      >> scaffolds/hgcA_scaffolds.fna
+  grep -A 1 -m 1 $scaffold\$ ~/BLiMMP/dataEdited/assemblies/scaffolds/$assemblyName\_assembly.fna \
+      >> working_directory/scaffolds/hgcA_scaffolds.fna
   echo "Pulling out" $scaffold "GFF entries"
   awk -v scaffold="$scaffold" '{ if ($1 == scaffold) print }' ~/BLiMMP/dataEdited/assemblies/ORFs/$assemblyName.gff \
-      >> scaffolds/hgcA_scaffolds.gff
+      >> working_directory/scaffolds/hgcA_scaffolds.gff
 done
 
 
@@ -121,31 +123,32 @@ PYTHONPATH=''
 #IFS=$'\n'
 scripts=~/BLiMMP/code
 ORFs=~/BLiMMP/dataEdited/assemblies/ORFs
-cd ~/BLiMMP/dataEdited/hgcA_analysis
-mkdir hgcB
-rm -f hgcB/downstream_gene_list.txt
+cd ~/BLiMMP/dataEdited/ABA/hgcA
+mkdir working_directory/hgcB
+rm -f working_directory/hgcB/downstream_gene_list.txt
 python $scripts/retrieve_downstream_gene_name.py \
-          identification/hgcA_good.txt \
-          scaffolds/hgcA_scaffolds.gff \
-          hgcB/downstream_gene_list.txt
+          hgcA_genes.txt \
+          working_directory/scaffolds/hgcA_scaffolds.gff \
+          working_directory/hgcB/downstream_gene_list.txt
 conda deactivate
 
 # Extract downstream amino acid sequences
-rm -f hgcB/downstream_genes.faa
-cat hgcB/downstream_gene_list.txt | while read gene
+cd working_directory/hgcB
+rm -f downstream_genes.faa
+cat downstream_gene_list.txt | while read gene
 do
   assemblyName=$(echo $gene | cut -d "_" -f1,2)
   echo "Pulling out" $gene "faa entry from" $assemblyName
-  grep -A 1 \
+  grep -A 1 -m 1 \
         $gene$ \
         $ORFs/$assemblyName.faa \
-      >> hgcB/downstream_genes.faa
+      >> downstream_genes.faa
 done
-grep '>' hgcB/downstream_genes.faa | \
-  sed 's/>//' > hgcB/downstream_genes_present.txt
+grep '>' downstream_genes.faa | \
+  sed 's/>//' > downstream_genes_present.txt
 
 # Search adjacent genes with hgcB HMM
-cd ~/BLiMMP/dataEdited/hgcA_analysis/hgcB
+cd ~/BLiMMP/dataEdited/ABA/hgcA/working_directory/hgcB
 source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
 conda activate bioinformatics
 PYTHONPATH=''
@@ -172,34 +175,25 @@ hmmalign --amino \
          ~/references/hgcA/hgcB_5M.HMM \
          hgcB.faa
 # Convert alignment to fasta format
-python $scripts/convert_stockhold_to_fasta.py hgcB.sto
-python $scripts/cleanFASTA.py hgcB.afa
-mv -f hgcB.afa_tempCleanedFile hgcB.afa
+python ~/BLiMMP/code/convert_stockhold_to_fasta.py hgcB.sto
+python ~/BLiMMP/code/cleanFASTA.py hgcB.afa
+mv -f hgcB.afa_temp.fasta hgcB.afa
 cat hgcB.afa | while read line; do echo ${line^^} >> hgcB_clean.afa; done
 sed -i 's/_ASSEMBLY/_assembly/' hgcB_clean.afa
 sed -i 's/_COASSEMBLY/_coassembly/' hgcB_clean.afa
-# Check sequences in alignment_hgcB.R.
+cp hgcB_clean.afa ../../
+# Check sequences in hgcA_data_processing.Rmd, on my local computer.
 
 # Check GFF entries for cut hgcB seqs
-cd ~/BLiMMP/dataEdited/hgcA_analysis/scaffolds
-cat hgcA_scaffolds.gff
-
+cd ~/BLiMMP/dataEdited/ABA/hgcA/working_directory/scaffolds
+declare -a hgcB_trunc=("BLI20_assembly005_000000188864_1" "BLI21_coassembly_000000421460_3" "BLI21_coassembly_000000483203_1" "BLI20_assembly001_000000070404_2" "BLI21_coassembly_000001026396_1" "BLI20_coassembly_000000275727_4")
+for gene_id in "${hgcB_trunc[@]}"
+do
+  scaffold_id=$(echo $gene_id | cut -d '_' -f 1-3)
+  grep $scaffold_id hgcA_scaffolds.gff
+done
 # They all check out, so I'll just use the hgcB.faa
 # file as our final set of hgcB seqs.
-cd ~/BLiMMP/dataEdited/hgcA_analysis/hgcB
-grep '>' hgcB.faa | \
-  sed 's/>//' > hgcB.txt
-
-# Pull out non-hgcB downstream genes
-cd ~/BLiMMP/dataEdited/hgcA_analysis/hgcB
-cat downstream_genes_present.txt hgcB.txt | \
-  sort | \
-  uniq -u | while read gene
-  do
-    grep -A 1 $gene downstream_genes.faa
-  done
-
-
 
 
 ######################
@@ -207,107 +201,27 @@ cat downstream_genes_present.txt hgcB.txt | \
 ######################
 
 screen -S BLI_hgcA_gene_neighborhood
-cd ~/BLiMMP/dataEdited/hgcA_analysis
+cd ~/BLiMMP/dataEdited/ABA/hgcA
 source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
 conda activate py_viz
 PYTHONPATH=''
 scripts=~/BLiMMP/code
 
-cat identification/hgcA_good.txt | while read hgcA_id
+cat ~/BLiMMP/dataEdited/ABA/hgcA/hgcA_genes.txt | while read hgcA_id
 do
   scaffold_id=$(echo $hgcA_id | cut -d '_' -f 1-3)
   echo "Working on" $hgcA_id", on scaffold" $scaffold_id
-  awk -F '\t' -v scaffold_id="$scaffold_id" '$1 == scaffold_id { print $0 }' scaffolds/hgcA_scaffolds.gff > scaffolds/temp_scaffolds.gff
+  awk -F '\t' -v scaffold_id="$scaffold_id" '$1 == scaffold_id { print $0 }' working_directory/scaffolds/hgcA_scaffolds.gff > working_directory/scaffolds/temp_scaffolds.gff
   gene_id=$(echo $hgcA_id | \
               cut -d '_' -f 3-4 | \
               sed 's/^0*//g')
   echo "Searching for" $gene_id
-  python $scripts/gene_neighborhood_extraction.py scaffolds/temp_scaffolds.gff \
-                                                  scaffolds/hgcA_scaffolds.fna \
+  python $scripts/gene_neighborhood_extraction.py working_directory/scaffolds/temp_scaffolds.gff \
+                                                  working_directory/scaffolds/hgcA_scaffolds.fna \
                                                   $gene_id \
                                                   5000 \
-                                                  scaffolds/temp_$gene_id
+                                                  working_directory/scaffolds/temp_$gene_id
 
-  rm -f scaffolds/temp_scaffolds.gff
+  rm -f working_directory/scaffolds/temp_scaffolds.gff
 done
 
-cd scaffolds
-cat temp_*.gff > hgcA_geneNeighborhood_raw.gff
-cat temp_*.fna > hgcA_geneNeighborhood_raw.fna
-rm -f *_neighborhood.*
-
-
-
-
-############################################
-############################################
-# Phylogenetic analysis of hgcA
-############################################
-############################################
-
-#########################
-# Generate good tree with Hg-MATE seqs using RAxML
-#########################
-
-# Done locally
-BLiMMP
-cp /Users/benjaminpeterson/Documents/research/Hg_MATE/versions/v1.01142021/Hg-MATE-Db.v1.01142021_ISOCELMAG_Hgc.fas \
-    references
-rm -f dataEdited/hgcA_analysis/phylogeny/raxml/HgMate_reference_seqs_to_use.faa
-cut -d"_" -f1-3 dataEdited/hgcA_analysis/phylogeny/raxml/reference_names_to_use.txt | while read reference_name
-do
-  grep -A 1 \
-    $reference_name \
-    references/Hg-MATE-Db.v1.01142021_ISOCELMAG_Hgc.fas \
-    >> dataEdited/hgcA_analysis/phylogeny/raxml/HgMate_reference_seqs_to_use.faa
-done
-
-
-# Done on GLBRC
-screen -S BLI_hgcA_tree
-source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
-conda activate bioinformatics
-PYTHONPATH=""
-PERL5LIB=''
-cd ~/BLiMMP/dataEdited/hgcA_analysis/phylogeny/raxml
-# Upload needed references
-cat 5M_bin_seqs.faa \
-    HgMate_reference_seqs_to_use.faa \
-    jones_hgcA_seqs.faa \
-    ../hgcA_for_phylogeny.faa \
-    hgcA_paralogs_for_rooting.faa \
-    > hgcA_for_tree.faa
-
-# Generate alignment
-muscle -in hgcA_for_tree.faa \
-        -out hgcA_for_tree.afa
-
-# Upload masked alignment
-# Then run RAxML to generate tree
-raxml=/opt/bifxapps/raxml-8.2.11/raxmlHPC-PTHREADS
-$raxml -f a \
-        -p 283976 \
-        -m PROTGAMMAAUTO \
-        -N autoMRE \
-        -x 2381 \
-        -T 20 \
-        -s hgcA_for_tree_masked.afa \
-        -n hgcA
-
-
-# Check out sequence similarity of study seqs to references
-cd ~/BLiMMP/dataEdited/hgcA_analysis
-mkdir derep_references
-cdhit=~/programs/cdhit-master
-$cdhit/cd-hit -g 1 \
-              -i phylogeny/raxml/hgcA_for_tree.faa \
-              -o derep_references/hgcA_ref.faa \
-              -c 0.97 \
-              -n 5 \
-              -d 0
-$cdhit/cd-hit -g 1 \
-              -i phylogeny/raxml/hgcA_for_tree.faa \
-              -o derep_references/hgcA_ref_80.faa \
-              -c 0.80 \
-              -n 5 \
-              -d 0
