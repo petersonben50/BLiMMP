f80d58780e97ca005836dbbec4ebeb081251d579
Benjamin D. Peterson <petersonben50@gmail.com>
2024-06-28 21:15:12 -0700

    new version of this figure

--- code/figures/incubations_k_discrepancy.R
+++ code/figures/incubations_k_discrepancy.R
@@ -12,42 +12,78 @@ library(tidyverse)
 cb.translator <- readRDS("references/colorblind_friendly_colors.rds")
 
 
-#### Read in and prepare data ####
+#### Read in and prepare incubation data ####
 incubation_data <- read.csv('dataFinal/incubation_Hg_rate_data.csv') %>%
   rename(date = startDate,
          Kmet = Kmet_int_t2,
          Kdem = Kdem_slope) %>%
   select(date, depth, treatment, Kmet, Kdem) %>%
-  filter(!(date == "2020-10-10" & depth == 15.7)) %>%
-  group_by(date, depth, treatment) %>%
-  summarise(counts = n(),
-            Kmet_mean = mean(Kmet),
-            Kmet_sem = sd(Kmet)/sqrt(counts),
-            Kdem_mean = mean(Kdem),
-            Kdem_sem = sd(Kdem)/sqrt(counts)) %>%
-  ungroup() %>%
-  select(-counts)
-geochem_data <- read.csv("dataFinal/water_chem_data.csv") %>%
-  filter(!is.na(sulfate_uM),
-         !corewater) %>%
+  filter(!(date == "2020-10-10" & depth == 15.7))
+
+incubation_data$Kdem[incubation_data$Kdem <= 0.005] <- 0.005
+incubation_data$Kmet[incubation_data$Kmet <= 0.001] <- 0.001
+
+ratio_data_rates <- incubation_data %>%
+  filter(treatment == "unfiltered-unamended") %>%
+  mutate(K_ratio = Kmet / Kdem) %>%
+  group_by(date, depth) %>%
+  summarize(K_ratio_mean = mean(K_ratio),
+            K_ratio_sd = sd(K_ratio),
+            K_ratio_n = n(),
+            K_ratio_sem = K_ratio_sd / sqrt(K_ratio_n)) %>%
+  select(date, depth, K_ratio_mean, K_ratio_sem) %>%
+  ungroup()
+
+
+#### Read in and prepare concentration data ####
+ratio_data_conc <- read.csv("dataFinal/water_chem_data.csv") %>%
+  filter(!is.na(FMHG_NG.L)) %>%
   group_by(date, depth) %>%
   summarise(FMHG_NG.L = mean(FMHG_NG.L),
             FiHg_NG.L = mean(FiHg_NG.L)) %>%
-  ungroup()
-all_data <- left_join(incubation_data,
-                      geochem_data)
-rm(geochem_data, incubation_data)
+  ungroup() %>%
+  mutate(conc_ratio = FMHG_NG.L / FiHg_NG.L) %>%
+  select(date, depth, conc_ratio)
 
 
-#### Calculate ratios ####
-ratio_data <- all_data %>%
-  filter(treatment == "unfiltered-unamended") 
-ratio_data$Kdem_mean[ratio_data$Kdem_mean < 0.01] <- 0.01
-ratio_data <- ratio_data %>%
-  mutate(conc_ratio = FMHG_NG.L / FiHg_NG.L,
-         K_ratio = Kmet_mean / Kdem_mean,
-         K_discrepancy = K_ratio / conc_ratio) %>%
-  select(date, depth, K_discrepancy, conc_ratio, K_ratio, FMHG_NG.L, FiHg_NG.L, Kmet_mean, Kdem_mean)
+#### Combine ratio data ####
+all_data <- left_join(ratio_data_rates,
+                      ratio_data_conc)
+rm(incubation_data,
+   ratio_data_rates,
+   ratio_data_conc)
+
+
+#### Set up vectors ####
+year_vector <- c(21, 24)
+names(year_vector) <- c("2020", "2021")
+
+
+#### Function to plot data ####
+plot_ratio_data <- function() {
+  plot(y = log(all_data$conc_ratio, 10),
+       x = log(all_data$K_ratio_mean, 10),
+       xlim = c(-1.5, 1.5),
+       ylim = c(-1.0, 1.0),
+       pch = year_vector[substr(all_data$date, 1, 4)],
+       col = "grey25",
+       bg = "grey85",
+       cex = 1.8,
+       # cex = sqrt(ratio_data$sulfide_uM)/10,
+       ylab = expression("MeHg / Hg(II) ratio"),
+       xlab = expression('K'['met']*' /  K'['dem']*' ratio'))
+  arrows(x0 = log((all_data$K_ratio_mean - all_data$K_ratio_sem), 10),
+         x1 = log((all_data$K_ratio_mean + all_data$K_ratio_sem), 10),
+         y0 = log(all_data$conc_ratio, 10), y1 = log(all_data$conc_ratio, 10),
+         angle = 90, code = 3, length = 0.05)
+  abline(a = 0, b = 1)
+  legend(x = -1.35, y = 0.9,
+         legend = names(year_vector),
+         pch = year_vector,
+         col = "grey25",
+         pt.bg = "grey85")
+}
+
 
 
 #### Generate plot ####
@@ -61,12 +97,5 @@ par(mfrow = c(1, 1),
     mgp = c(2, 0.4, 0),
     cex.axis = 1.1,
     cex.lab = 1.2)
-plot(x = ratio_data$Kmet_mean,
-     y = log(ratio_data$K_discrepancy, 10),
-     xlim = c(0, 0.2),
-     ylim = c(-1.2, 1.2),
-     xlab = expression('K'['met']*' (day'^-1*')'),
-     ylab = expression('(K'['met']*' /  K'['dem']*') / (MeHg / Hg(II))'),
-     pch = 16,
-     cex = 1.2)
+plot_ratio_data()
 dev.off()
